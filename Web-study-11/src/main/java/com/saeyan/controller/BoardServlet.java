c9c4927b6f45e32d04189716e3d809a0820d2dc6
Jade_Rain <83251008+jaderain765@users.noreply.github.com>
2022-09-02 20:32:55 +0900

    커밋

--- Web-study-11/src/main/java/com/saeyan/controller/BoardServlet.java
+++ Web-study-11/src/main/java/com/saeyan/controller/BoardServlet.java
@@ -7,6 +7,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.saeyan.controller.action.Action;
+
 /**
  * Servlet implementation class BoardServlet
  */
@@ -28,6 +30,13 @@ public class BoardServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String command = request.getParameter("command");
 		System.out.println("BoardServlet에서 요청을 받음을 확인 : "+ command);
+		
+		ActionFactory af = ActionFactory.getInstance();
+		Action action = af.getAction(command);
+		
+		if(action != null) {
+			action.execute(request, response);
+		}
 	}
 
 	/**
